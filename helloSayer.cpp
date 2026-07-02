c93148cf74e5127907223160e7b9dbb93ea3d876
Robyn Dizon <rdizon@g.hmc.edu>
2026-07-02 12:32:25 -0700

    Create helloSayer.cpp

---
+++ helloSayer.cpp
@@ -0,0 +1,10 @@
+/*
+ * This file contains implementation for the HelloSayer class
+ */
+
+#include "helloSayer.hpp"
+#include <iostream>
+
+void HelloSayer::sayHello() const {
+    std::cout << "Hello from the HelloSayer class" << std::endl;
+}
